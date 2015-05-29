5b16e009e22daa4317b9bcac1e82968f1dbfc262
marrygrim <marrygrim@gmail.com>
2015-05-29 18:47:29 -0500

    added more possable commands and programs

--- prep.sh
+++ prep.sh
@@ -1,2 +1,14 @@
 echo "\n| rememer to close this window after this download of steam\n|"&& sleep 5
 steam
+
+
+sudo apt-get install mumble
+
+
+sudo apt-get update;sudo apt-get dist-update
+echo "\n updating\n" 
+
+sudo add-apt-repository ppa:plushuang-tw/uget-stable
+sudo apt-get install uget
+sudo apt-get install aria2
+sudo apt-get update;sudo apt-get dist-upgrade
